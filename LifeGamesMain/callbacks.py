237a25f48480d385eb234ff3c104982c2d00636d
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-10 23:37:21 +0530

    Update callbacks.py

--- LifeGamesMain/callbacks.py
+++ LifeGamesMain/callbacks.py
@@ -17,15 +17,25 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
                 chat_id=chat_id,
                 message_id=message_id,
                 text=Data.START.format(callback_query.from_user.mention, mention),
-                reply_markup=InlineKeyboardMarkup(Keshav.alpha_buttons),
+                reply_markup=InlineKeyboardMarkup(Keshav.home_button),
             )
-    elif query == "about":
+    elif query == "credits":
         chat_id = callback_query.from_user.id
         message_id = callback_query.message.message_id
         await bot.edit_message_text(
             chat_id=chat_id,
             message_id=message_id,
-            text=Data.ABOUT,
+            text=Keshav.CREDITS,
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(Data.home_buttons),
+            reply_markup=InlineKeyboardMarkup(Keshav.home_button),
+        )
+    elif query == "cmda":
+        chat_id = callback_query.from_user.id
+        message_id = callback_query.message.message_id
+        await bot.edit_message_text(
+            chat_id=chat_id,
+            message_id=message_id,
+            text=Keshav.CMDA,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(Keshav.home_button),
         )
