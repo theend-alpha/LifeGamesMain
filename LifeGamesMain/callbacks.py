a7fd6af27f671e474d894265f0f6f9ee33c68ccc
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-10 23:29:39 +0530

    Update callbacks.py

--- LifeGamesMain/callbacks.py
+++ LifeGamesMain/callbacks.py
@@ -9,11 +9,23 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
     # user_id = callback_query.from_user.id
     mention = user["mention"]
     query = callback_query.data.lower()
-if query == "update":
+    if query.startswith("home"):
+        if query == 'home':
+            chat_id = callback_query.from_user.id
+            message_id = callback_query.message.message_id
+            await bot.edit_message_text(
+                chat_id=chat_id,
+                message_id=message_id,
+                text=Data.START.format(callback_query.from_user.mention, mention),
+                reply_markup=InlineKeyboardMarkup(Keshav.alpha_buttons),
+            )
+    elif query == "about":
         chat_id = callback_query.from_user.id
         message_id = callback_query.message.message_id
         await bot.edit_message_text(
             chat_id=chat_id,
             message_id=message_id,
-            text=Keshav.CMDA,
+            text=Data.ABOUT,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(Data.home_buttons),
         )
