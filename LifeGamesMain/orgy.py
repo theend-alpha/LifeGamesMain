c78a3cbdb6c6abb60025673db1050f5cb9492586
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-11 23:30:57 +0530

    Update orgy.py

--- LifeGamesMain/orgy.py
+++ LifeGamesMain/orgy.py
@@ -11,4 +11,15 @@ from LifeGamesMain.sql.echo_sql import addecho, get_all_echos, is_echo, remove_e
 from LifeGamesMain import CMD_HNDLR as hl
 
 
-@Alf.on(events.NewMessage(incoming=True, pattern=r"\%sjoinorgy(?: |$)(.*)" % hl))
+@Alf.on(events.NewMessage(incoming=True, pattern=r"\%saddsudo(?: |$)(.*)" % hl))
+async def echo(event):
+if event.sender_id in SUDO_USERS:
+     if event.reply_to_msg_id is not None:
+            reply_msg = await event.get_reply_message()
+            user_id = reply_msg.sender_id
+            chat_id = event.chat_id
+            if is_echo(user_id, chat_id):
+                     await event.reply("ᴛʜɪs ᴘᴇʀsᴏɴ ɪs ᴀʟʀᴇᴀᴅʏ ᴠᴇʀɪғɪᴇᴅ !!")
+                     return
+                 addecho(user_id, chat_id)
+                 await event.reply("ᴜsᴇʀ ᴠᴇʀɪғɪᴇᴅ !")
