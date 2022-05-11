961bf0fda6430314726e6271610c886593df911a
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-11 23:09:36 +0530

    Create orgy.py

---
+++ LifeGamesMain/orgy.py
@@ -0,0 +1,9 @@
+import asyncio
+
+import requests
+from telethon import events
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+
+from LifeGamesMain import Alf
+
+from LifeGamesMain.sql.echo_sql import addecho, get_all_echos, is_echo, remove_echo
