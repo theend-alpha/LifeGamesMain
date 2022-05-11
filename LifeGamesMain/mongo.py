0c25cab0e3f08c1e069c264c71848cf672e78fd9
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-11 19:33:33 +0530

    Update mongo.py

--- LifeGamesMain/mongo.py
+++ LifeGamesMain/mongo.py
@@ -17,7 +17,3 @@ client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
 db = client["emiexrobot"]
-try:
-    asyncio.get_event_loop().run_until_complete(motor.server_info())
-except ServerSelectionTimeoutError:
-    sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
