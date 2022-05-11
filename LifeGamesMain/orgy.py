76dba5263917988990de4dab1184d01487853546
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-11 23:16:09 +0530

    Update orgy.py

--- LifeGamesMain/orgy.py
+++ LifeGamesMain/orgy.py
@@ -7,3 +7,5 @@ from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 from LifeGamesMain import Alf
 
 from LifeGamesMain.sql.echo_sql import addecho, get_all_echos, is_echo, remove_echo
+
+from LifeGamesMain import CMD_HNDLR as hl
