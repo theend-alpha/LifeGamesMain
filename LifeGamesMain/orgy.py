4e8437f89b540d4ce8cc2592f8ea55f9a34c347c
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-11 23:33:42 +0530

    Update orgy.py

--- LifeGamesMain/orgy.py
+++ LifeGamesMain/orgy.py
@@ -10,10 +10,11 @@ from LifeGamesMain.sql.echo_sql import addecho, get_all_echos, is_echo, remove_e
 
 from LifeGamesMain import CMD_HNDLR as hl
 
+OWNER_ID = 1927705508
 
 @Alf.on(events.NewMessage(incoming=True, pattern=r"\%saddsudo(?: |$)(.*)" % hl))
 async def echo(event):
-if event.sender_id in SUDO_USERS:
+if event.sender_id in OWNER_ID:
      if event.reply_to_msg_id is not None:
             reply_msg = await event.get_reply_message()
             user_id = reply_msg.sender_id
@@ -23,3 +24,6 @@ if event.sender_id in SUDO_USERS:
                      return
                  addecho(user_id, chat_id)
                  await event.reply("ᴜsᴇʀ ᴠᴇʀɪғɪᴇᴅ !")
+
+else:
+       await event.reply("vmro 😑😑")
