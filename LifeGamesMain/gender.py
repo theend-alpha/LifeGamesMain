c02cb4c187eb41011e7d39f3f120ea87505bc623
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-13 00:56:47 +0530

    Update gender.py

--- LifeGamesMain/gender.py
+++ LifeGamesMain/gender.py
@@ -12,3 +12,15 @@ from Yashvi import Keshav
 async def gender(_, message: Message):
         await message.reply("start bot --> settings --> gender ",
                             reply_markup=InlineKeyboardMarkup(Keshav.gender_button))
+
+
+@Client.on_message(
+      filters.command("gender@nothehe_bot")
+    & filters.group
+    & ~filters.edited
+    & ~filters.forwarded
+    & ~filters.via_bot
+)
+async def gendero(_, message: Message):
+        await message.reply("start bot --> settings --> gender ",
+                            reply_markup=InlineKeyboardMarkup(Keshav.gender_button))
