fb7667c1ed9e5ad721d6bb12ad63693886541038
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-12 22:45:38 +0530

    Update gender.py

--- LifeGamesMain/gender.py
+++ LifeGamesMain/gender.py
@@ -1,2 +1,13 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, Message
+
+@Client.on_message(
+      filters.command("gender")
+    & filters.group
+    & ~filters.edited
+    & ~filters.forwarded
+    & ~filters.via_bot
+)
+async def gender(_, message: Message):
+        await message.reply("Click [here](t.me/nothehe_bot) to change gender",
+                            disable_web_page_preview=True)
