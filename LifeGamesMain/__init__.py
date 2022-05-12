a0dc37c245332cd094f62deee500d299809165d2
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-12 12:56:35 +0530

    Update __init__.py

--- LifeGamesMain/__init__.py
+++ LifeGamesMain/__init__.py
@@ -11,3 +11,14 @@ from pyrogram.types import Message
 from pyrogram import Client, errors
 from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid, ChannelInvalid
 from pyrogram.types import Chat, User
+
+
+LOGGER = logging.getLogger(__name__)
+
+alf = Client(
+    ":memory:",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    workers=min(32, os.cpu_count() + 4),
+)
