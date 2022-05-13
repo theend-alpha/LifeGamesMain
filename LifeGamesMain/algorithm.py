131812d764e22013e2a01b5b99bb04d4ab671521
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-13 11:55:16 +0530

    Update algorithm.py

--- LifeGamesMain/algorithm.py
+++ LifeGamesMain/algorithm.py
@@ -1,1 +1,26 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, Message
+from Yashvi import Keshav
 
+@Client.on_message(
+      filters.command("algorithm")
+    & filters.group
+    & ~filters.edited
+    & ~filters.forwarded
+    & ~filters.via_bot
+)
+async def gender(_, message: Message):
+        await message.reply(Keshav.ALGO,
+                            reply_markup=InlineKeyboardMarkup(Keshav.hexa_button))
+
+
+@Client.on_message(
+      filters.command("algorithm@nothehe_bot")
+    & filters.group
+    & ~filters.edited
+    & ~filters.forwarded
+    & ~filters.via_bot
+)
+async def gendero(_, message: Message):
+        await message.reply(Keshav.ALGO,
+                            reply_markup=InlineKeyboardMarkup(Keshav.hexa_button))
