8017ce30c01a394c05e7e5904eb0b7f8e7c212b5
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-15 23:56:21 +0530

    Update smooch.py

--- LifeGamesMain/smooch.py
+++ LifeGamesMain/smooch.py
@@ -2,7 +2,7 @@ import os
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, Message
-
+from Alone import AlphaIsAlone
 
 
 
@@ -15,3 +15,12 @@ async def info_func(_, message: Message):
         target_id = message.from_user.id
     elif not message.reply_to_message and len(message.command) != 1:
         target_id = message.text.split(None, 1)[1]
+
+    bsdk_id = message.from_user.id
+
+    user = await _.get_me()
+    mention = user["mention"]
+    await _.send_message(
+      message.chat.id,
+      AlphaIsAlone.SMOOCH.format(message.from_user.mention, message.reply_to_message.from_user.mention),
+      reply_markup=InlineKeyboardMarkup(AlphaIsAlone.smooch_buttons))
