2518b908037b23f9826429e2d7f6a7a732c7bc0d
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-16 11:52:09 +0530

    Update smooch.py

--- LifeGamesMain/smooch.py
+++ LifeGamesMain/smooch.py
@@ -9,27 +9,15 @@ from Alone import AlphaIsAlone
 
 @Client.on_message(filters.command("smooch"))
 async def info_func(_, message: Message):
-    if message.reply_to_message:
-        target_id = message.reply_to_message.from_user.id
-    elif not message.reply_to_message and len(message.command) == 1:
-        target_id = message.from_user.id
-    elif not message.reply_to_message and len(message.command) != 1:
-        target_id = message.text.split(None, 1)[1]
-
-    TARGET_ID = "target_id"
-
+ 
+    target_id = message.reply_to_message.from_user.id
+   
     user = await _.get_me()
     mention = user["mention"]
     await _.send_message(
       message.chat.id,
       AlphaIsAlone.SMOOCH.format(message.from_user.mention, message.reply_to_message.from_user.mention),
       reply_markup=InlineKeyboardMarkup(AlphaIsAlone.smooch_buttons))
-
-from Data import Data
-from Yashvi import Keshav
-from pyrogram import Client
-from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton, Message
-from Alone import AlphaIsAlone
 
 smooch = "https://te.legra.ph/file/2bd00c6b47f9f3a7bfe1d.jpg"
 
@@ -42,7 +30,7 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
    
     if query == "smooch":
         chat_id = callback_query.from_user.id
-        if TARGET_ID == chat_id
+        if target_id == chat_id
         await callback_query.message.reply_photo(smooch,
                                                  caption=AlphaIsAlone.SMOOCHA.format(message.reply_to_message.from_user.mention, message.from_user.mention))
            
