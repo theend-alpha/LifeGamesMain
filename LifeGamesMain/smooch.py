167c10aff39eff466f4bc3f5c8bda589d02743a7
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-16 12:21:46 +0530

    Update smooch.py

--- LifeGamesMain/smooch.py
+++ LifeGamesMain/smooch.py
@@ -1,7 +1,7 @@
 import os
 
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, Message, CallbackQuery
+from pyrogram.types import InlineKeyboardMarkup, Message
 from Alone import AlphaIsAlone
 
 
@@ -18,18 +18,3 @@ async def info_func(_, message: Message):
       message.chat.id,
       AlphaIsAlone.SMOOCH.format(message.from_user.mention, message.reply_to_message.from_user.mention),
       reply_markup=InlineKeyboardMarkup(AlphaIsAlone.smooch_buttons))
-
-smooch = "https://te.legra.ph/file/2bd00c6b47f9f3a7bfe1d.jpg"
-
-@Client.on_callback_query()
-async def _callbacks(bot: Client, callback_query: CallbackQuery):
-    user = await bot.get_me()
-    # user_id = callback_query.from_user.id
-    mention = user["mention"]
-    query = callback_query.data.lower()
-   
-    if query == "smooch":
-        chat_id = callback_query.from_user.id
-        await callback_query.message.reply_photo(smooch,
-                                                 caption=AlphaIsAlone.SMOOCHA.format(message.reply_to_message.from_user.mention, message.from_user.mention))
-           
