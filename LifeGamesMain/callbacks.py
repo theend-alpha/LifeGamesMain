4d283edb180ccee54dcec3f8a453662a288ec36d
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-16 12:21:02 +0530

    Update callbacks.py

--- LifeGamesMain/callbacks.py
+++ LifeGamesMain/callbacks.py
@@ -3,6 +3,8 @@ from Yashvi import Keshav
 from pyrogram import Client
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton, Message
 
+smooch = "https://te.legra.ph/file/2bd00c6b47f9f3a7bfe1d.jpg"
+
 @Client.on_callback_query()
 async def _callbacks(bot: Client, callback_query: CallbackQuery):
     user = await bot.get_me()
@@ -40,4 +42,10 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(Keshav.home_button),
         )
+    elif query == "smooch":
+        await callback_query.message.reply_photo(smooch,
+            caption=AlphaIsAlone.SMOOCHA.format(message.reply_to_message.from_user.mention, message.from_user.mention))
+    elif query == "reject":
+        await callback_query.message.reply_photo(smooch,
+            caption=AlphaIsAlone.SMOOCHA.format(message.reply_to_message.from_user.mention, message.from_user.mention))
     
