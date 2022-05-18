e882971bc35963524926b75757e68ad28218cb19
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-18 22:01:59 +0530

    Update callbacks.py

--- LifeGamesMain/callbacks.py
+++ LifeGamesMain/callbacks.py
@@ -46,7 +46,7 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
     elif query == "smooch":
         chat_id = callback_query.from_user.id 
           if chat_id in TARGET_ID
-            await callback_query.message.reply_photo(smooch,
+              await callback_query.message.reply_photo(smooch,
               caption=AlphaIsAlone.SMOOCHA)
     elif query == "reject":
         await callback_query.message.reply_photo(smooch,
