df0b78aaa8a203e43c75fff89db598dce9d709c1
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-19 01:08:06 +0530

    Update callbacks.py

--- LifeGamesMain/callbacks.py
+++ LifeGamesMain/callbacks.py
@@ -53,12 +53,12 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
             await bot.edit_message_text(
                     chat_id=chat_id,
                     message_id=message_id,
-                    text="accepted !")
+                    text=accepted !)
     elif query == "reject":
         chat_id = callback_query.from_user.id 
         message_id = callback_query.message.message_id
         await bot.edit_message_text(
                 chat_id=chat_id,
                 message_id=message_id,
-                text="accepted !")
+                text=rejected !)
     
