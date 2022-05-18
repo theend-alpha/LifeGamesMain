46bb53c6c91af6176b5a41fe9cdf11a7b55ac3de
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-18 10:25:48 +0530

    Update smooch.py

--- LifeGamesMain/smooch.py
+++ LifeGamesMain/smooch.py
@@ -5,3 +5,18 @@ from telethon.tl.functions.users import GetFullUserRequest
 
 @ALF.on(events.NewMessage(incoming=True, pattern="/smooch")
 async def smooch(event):
+
+
+async def get_user(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.forward:
+            replied_user = await event.client(
+                GetFullUserRequest(previous_message.forward.sender_id)
+            )
+        else:
+            replied_user = await event.client(
+                GetFullUserRequest(previous_message.sender_id)
+            )
+    target = replied_user.user.id
+    return target
