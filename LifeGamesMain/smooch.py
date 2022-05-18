cf04eb40cd3691c93230eb9e3f9afedeaf51c20a
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-18 10:23:52 +0530

    Update smooch.py

--- LifeGamesMain/smooch.py
+++ LifeGamesMain/smooch.py
@@ -1,20 +1,7 @@
-import os
+from telethon import events, version, Button
+from telethon.tl.custom import button
+from LifeGamesMain import ALF
+from telethon.tl.functions.users import GetFullUserRequest
 
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, Message
-from Alone import AlphaIsAlone
-
-
-
-
-@Client.on_message(filters.command("smooch"))
-async def info_func(_, message: Message):
- 
-    target_id = message.reply_to_message.from_user.id
-   
-    user = await _.get_me()
-    mention = user["mention"]
-    await _.send_message(
-      message.chat.id,
-      AlphaIsAlone.SMOOCH.format(message.from_user.mention, message.reply_to_message.from_user.mention),
-      reply_markup=InlineKeyboardMarkup(AlphaIsAlone.smooch_buttons))
+@ALF.on(events.NewMessage(incoming=True, pattern="/smooch")
+async def smooch(event):
