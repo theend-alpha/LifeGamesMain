89708f12c8f16bbf2a2673082e296f93aff574f7
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-27 15:24:18 +0530

    Update tara.py

--- LifeGamesMain/tara.py
+++ LifeGamesMain/tara.py
@@ -3,11 +3,11 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, Message, CallbackQuery
 
 @Client.on_message(filters.group & ~filters.edited & ~filters.forwarded & ~filters.via_bot & filters.command("tara"))
-async def start(bot, msg: Message):
+async def start(bot, message: Message):
 	user = await bot.get_me()
 	mention = user["mention"]
 	await bot.send_message(
-		msg.chat.id,
+		message.chat.id,
 		AlphaIsAlone.TARA.format(msg.from_user.mention, message.reply_to_message.from_user.mention),
                 disable_web_page_preview=True,
 		reply_markup=InlineKeyboardMarkup(AlphaIsAlone.tara_buttons)
@@ -15,5 +15,9 @@ async def start(bot, msg: Message):
 
 @Client.on_callback_query(filters.regex("reject"))
 async def rej(bot, query: CallbackQuery):
-    await query.msg.delete()
+    await query.message.delete()
+
+@Client.on_callback_query(filters.regex("tara"))
+async def acc(bot, query: CallbackQuery):
+    
 
