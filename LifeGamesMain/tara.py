6fd41d3313b22229f9cd58f52cf8a16667253d0d
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-28 13:42:05 +0530

    Update tara.py

--- LifeGamesMain/tara.py
+++ LifeGamesMain/tara.py
@@ -9,11 +9,11 @@ async def start(bot, message: Message):
     user = await bot.get_me()
     mention = user["mention"]
     target_id = message.reply_to_message.from_user.id
-	await bot.send_message(
-           message.chat.id,
-           AlphaIsAlone.TARA.format(msg.from_user.mention, message.reply_to_message.from_user.mention),
-           disable_web_page_preview=True,
-           reply_markup=InlineKeyboardMarkup(AlphaIsAlone.tara_buttons)
+    await bot.send_message(
+        message.chat.id,
+        AlphaIsAlone.TARA.format(msg.from_user.mention, message.reply_to_message.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(AlphaIsAlone.tara_buttons)
 	)
 
 @Client.on_callback_query(filters.regex("reject"))
