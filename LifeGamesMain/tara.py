743d8184f6a4c494faa9b1d6bdc4a0cec4a0eb25
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-28 13:17:19 +0530

    Update tara.py

--- LifeGamesMain/tara.py
+++ LifeGamesMain/tara.py
@@ -6,13 +6,14 @@ from pyrogram.types import InlineKeyboardMarkup, Message, CallbackQuery
 async def start(bot, message: Message):
 	user = await bot.get_me()
 	mention = user["mention"]
-        target_id = message.reply_to_message.from_user.id
 	await bot.send_message(
 		message.chat.id,
 		AlphaIsAlone.TARA.format(msg.from_user.mention, message.reply_to_message.from_user.mention),
                 disable_web_page_preview=True,
 		reply_markup=InlineKeyboardMarkup(AlphaIsAlone.tara_buttons)
 	)
+
+target_id = message.reply_to_message.from_user.id
 
 @Client.on_callback_query(filters.regex("reject"))
 async def rej(bot, query: CallbackQuery):
