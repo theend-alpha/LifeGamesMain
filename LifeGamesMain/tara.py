e3ff28211197d533ced2ad846d02524e2cc86927
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-28 13:30:09 +0530

    Update tara.py

--- LifeGamesMain/tara.py
+++ LifeGamesMain/tara.py
@@ -2,16 +2,18 @@ from Alone import AlphaIsAlone
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, Message, CallbackQuery
 
+TARGET_ID = []
+
 @Client.on_message(filters.group & ~filters.edited & ~filters.forwarded & ~filters.via_bot & filters.command("tara"))
 async def start(bot, message: Message):
-	user = await bot.get_me()
-	mention = user["mention"]
-        target_id = message.reply_to_message.from_user.id
+    user = await bot.get_me()
+    mention = user["mention"]
+    target_id = message.reply_to_message.from_user.id
 	await bot.send_message(
-		message.chat.id,
-		AlphaIsAlone.TARA.format(msg.from_user.mention, message.reply_to_message.from_user.mention),
-                disable_web_page_preview=True,
-		reply_markup=InlineKeyboardMarkup(AlphaIsAlone.tara_buttons)
+           message.chat.id,
+           AlphaIsAlone.TARA.format(msg.from_user.mention, message.reply_to_message.from_user.mention),
+           disable_web_page_preview=True,
+           reply_markup=InlineKeyboardMarkup(AlphaIsAlone.tara_buttons)
 	)
 
 @Client.on_callback_query(filters.regex("reject"))
