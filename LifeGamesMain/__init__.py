10257bd76d2c649d52dcf5d198f360b26cedee6c
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-02 16:26:02 +0530

    Update __init__.py

--- LifeGamesMain/__init__.py
+++ LifeGamesMain/__init__.py
@@ -1,5 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 import Config
 
-mongo = MongoClient(config.MONGO_DB_URI)
+mongo = MongoClient(Config.MONGO_DB_URI)
 db = mongo.AFK
