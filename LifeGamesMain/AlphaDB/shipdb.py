bcf25ca3b6b2ddc343f8d1fbc9487db752cd2574
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-02 18:09:16 +0530

    Create shipdb.py

---
+++ LifeGamesMain/AlphaDB/shipdb.py
@@ -0,0 +1,30 @@
+import codecs
+import pickle
+from typing import Dict, List, Union
+
+from LifeGamesMain.mongo import db
+
+coupledb = db.couple
+
+async def _get_lovers(chat_id: int):
+    lovers = coupledb.find_one({"chat_id": chat_id})
+    if not lovers:
+        return {}
+    return lovers["couple"]
+
+
+async def get_couple(chat_id: int, date: str):
+    lovers = await _get_lovers(chat_id)
+    if date in lovers:
+        return lovers[date]
+    return False
+
+
+async def save_couple(chat_id: int, date: str, couple: dict):
+    lovers = await _get_lovers(chat_id)
+    lovers[date] = couple
+    coupledb.update_one(
+        {"chat_id": chat_id},
+        {"$set": {"couple": lovers}},
+        upsert=True,
+    )
