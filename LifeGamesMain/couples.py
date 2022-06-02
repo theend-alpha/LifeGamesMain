c20ee81a9c4122825dcb62af8116d161fce05d5d
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-02 17:59:42 +0530

    Update couples.py

--- LifeGamesMain/couples.py
+++ LifeGamesMain/couples.py
@@ -1,1 +1,27 @@
+from EndAfk.AlphaDB.shipdb import get_couple, save_couple
+from pyrogram import filters
+import random
+from datetime import datetime
+
+
+def dt():
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M")
+    dt_list = dt_string.split(" ")
+    return dt_list
+
+
+def dt_tom():
+    a = (
+        str(int(dt()[0].split("/")[0]) + 1)
+        + "/"
+        + dt()[0].split("/")[1]
+        + "/"
+        + dt()[0].split("/")[2]
+    )
+    return a
+
+
+today = str(dt()[0])
+tomorrow = str(dt_tom())
 
