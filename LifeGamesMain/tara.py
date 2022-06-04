5587d12a70a9b486dfd885e2025a1135ad6f5532
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-05 00:12:57 +0530

    Update tara.py

--- LifeGamesMain/tara.py
+++ LifeGamesMain/tara.py
@@ -2,6 +2,8 @@ from Alone import AlphaIsAlone
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, CallbackQuery
 
+alpha = "https://te.legra.ph/file/2154fc8d07f429b2eeef6.jpg"
+
 @Client.on_message(filters.command("tara") & filters.group & ~filters.forwarded & ~filters.via_bot)
 async def tara(_, message: Message):
     global f_id
@@ -23,5 +25,10 @@ async def taraback(_: Client, query: CallbackQuery):
     resp_id = query.from_user.id
     if resp_id == f_id:
         if query.data == "tara":
-            
+            try:
+                await query.message.reply_photo(alpha, AlphaIsAlone.TARAA.format(f_m, i_m))
+            except:
+                return
+        elif query.data == "reject":
+            await query.message.delete()
         
